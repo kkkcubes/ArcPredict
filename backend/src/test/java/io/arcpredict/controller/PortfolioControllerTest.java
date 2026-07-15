d11af1970f06d8bae599cdebd2039fdcda5e9002
kkkcubes <kcube731@gmail.com>
2026-07-15 13:35:28 +0530

    Configure PortfolioControllerTest

--- backend/src/test/java/io/arcpredict/controller/PortfolioControllerTest.java
+++ backend/src/test/java/io/arcpredict/controller/PortfolioControllerTest.java
@@ -1,8 +1,37 @@
 package io.arcpredict.controller;
 
+import io.arcpredict.config.SecurityConfig;
+import io.arcpredict.repository.TradeRepository;
+import io.arcpredict.service.PortfolioService;
+
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.context.annotation.Import;
+
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(
+    PortfolioController.class
+)
+
+@Import(
+    SecurityConfig.class
+)
 class PortfolioControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PortfolioService portfolioService;
+
+    @MockBean
+    private TradeRepository tradeRepository;
 
     @Test
     void contextLoads() {
