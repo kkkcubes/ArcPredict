63adc906aa7a81a780c7c110a356b29be6fa65fb
kkkcubes <kcube731@gmail.com>
2026-07-15 13:17:57 +0530

    Test analytics endpoint returns HTTP 200

--- backend/src/test/java/io/arcpredict/controller/AnalyticsControllerTest.java
+++ backend/src/test/java/io/arcpredict/controller/AnalyticsControllerTest.java
@@ -1,15 +1,31 @@
 package io.arcpredict.controller;
 
+import io.arcpredict.config.SecurityConfig;
+import io.arcpredict.entity.AnalyticsEntity;
 import io.arcpredict.service.AnalyticsService;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import org.springframework.context.annotation.Import;
+
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(AnalyticsController.class)
+@WebMvcTest(
+    AnalyticsController.class
+)
+
+@Import(
+    SecurityConfig.class
+)
 class AnalyticsControllerTest {
 
     @Autowired
@@ -19,7 +35,24 @@ class AnalyticsControllerTest {
     private AnalyticsService analyticsService;
 
     @Test
-    void contextLoads() {
+    void shouldReturnAnalytics() throws Exception {
+
+        AnalyticsEntity analytics =
+            AnalyticsEntity.builder()
+                .build();
+
+        when(
+            analyticsService.getAnalytics()
+        ).thenReturn(
+            analytics
+        );
+
+        mockMvc.perform(
+                get("/api/analytics")
+            )
+            .andExpect(
+                status().isOk()
+            );
 
     }
 
