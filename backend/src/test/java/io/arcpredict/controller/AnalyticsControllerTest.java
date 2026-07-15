830f36041a732581e8af3d216397932face44f86
kkkcubes <kcube731@gmail.com>
2026-07-15 13:11:59 +0530

    Configure AnalyticsControllerTest

--- backend/src/test/java/io/arcpredict/controller/AnalyticsControllerTest.java
+++ backend/src/test/java/io/arcpredict/controller/AnalyticsControllerTest.java
@@ -1,8 +1,22 @@
 package io.arcpredict.controller;
 
+import io.arcpredict.service.AnalyticsService;
+
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(AnalyticsController.class)
 class AnalyticsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AnalyticsService analyticsService;
 
     @Test
     void contextLoads() {
