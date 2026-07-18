335500d0f387c5e8397fe4802cfbd113fab58c7d
kkkcubes <kcube731@gmail.com>
2026-07-18 08:32:09 +0530

    Enable Spring Retry support

--- backend/src/main/java/io/arcpredict/ArcPredictApplication.java
+++ backend/src/main/java/io/arcpredict/ArcPredictApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+import org.springframework.retry.annotation.EnableRetry;
+
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableRetry
 @ConfigurationPropertiesScan
 public class ArcPredictApplication {
 
